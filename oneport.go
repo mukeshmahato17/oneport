75e93f784c9e496e72bcb7989f18712cc4250bc1
mukeshmahato17 <mukeshmahato1089@gmail.com>
2025-12-22 21:06:26 +0545

    buffer added

--- oneport.go
+++ oneport.go
@@ -115,13 +115,38 @@ type muxListener struct {
 	doneChan chan struct{}
 }
 
-// MuxConn wraps the net.Conn and provide a transparent sniffing of connection data.
+// / MuxConn wraps a net.Conn and provides transparent sniffing of connection data.
 type MuxConn struct {
 	net.Conn
+	buf bufferedReader
 }
 
-func newMuxConn(conn net.Conn) *MuxConn {
+func newMuxConn(c net.Conn) *MuxConn {
 	return &MuxConn{
-		Conn: conn,
+		Conn: c,
+		buf:  bufferedReader{source: c},
 	}
+}
+
+// From the io.Reader documentation:
+//
+// When Read encounters an error or end-of-file condition after
+// successfully reading n > 0 bytes, it returns the number of
+// bytes read.  It may return the (non-nil) error from the same call
+// or return the error (and n == 0) from a subsequent call.
+// An instance of this general case is that a Reader returning
+// a non-zero number of bytes at the end of the input stream may
+// return either err == EOF or err == nil.  The next Read should
+// return 0, EOF.
+func (m *MuxConn) Read(p []byte) (int, error) {
+	return m.buf.Read(p)
+}
+
+func (m *MuxConn) startSniffing() io.Reader {
+	m.buf.reset(true)
+	return &m.buf
+}
+
+func (m *MuxConn) doneSniffing() {
+	m.buf.reset(false)
 }
